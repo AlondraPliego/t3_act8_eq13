9e12ddccb57524e96995cb750c897971a5fa014b
MinParís <gomezlizette360@gmail.com>
2026-07-14 08:45:08 -0600

    Agrega páginas y consumo de API

--- CRUD_MirateALGO!/src/components/MainContent/MainContent.jsx
+++ CRUD_MirateALGO!/src/components/MainContent/MainContent.jsx
@@ -1,14 +1,40 @@
+import Tendencias from "../../Pages/Tendencias";
+import Favoritos from "../../Pages/Favoritos";
+import MiLista from "../../Pages/MiLista";
+import Configuracion from "../../Pages/Configuracion";
+
 function MainContent({ seccion }) {
 
-    return (
+    switch (seccion) {
 
-        <div style={{ padding: "30px", color: "white" }}>
+        case "tendencias":
+            return <Tendencias />;
 
-            <h1>{seccion}</h1>
+        case "favoritos":
+            return <Favoritos />;
 
-        </div>
+        case "lista":
+            return <MiLista />;
 
-    );
+        case "configuracion":
+            return <Configuracion />;
+
+        default:
+            return (
+
+                <div style={{ padding: "30px" }}>
+
+                    <h1>🏠 Inicio</h1>
+
+                    <p>
+                        Aquí aparecerá la tabla CRUD realizada por el resto del equipo.
+                    </p>
+
+                </div>
+
+            );
+
+    }
 
 }
 
